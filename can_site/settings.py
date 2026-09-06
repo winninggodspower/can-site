93adc30d28ed272fbdc23c20813436c76087935a
winning godspower <winninggodspower@gmail.com>
2026-09-06 01:19:46 +0100

    update db to only use  postgresql when database_url is provided

--- can_site/settings.py
+++ can_site/settings.py
@@ -96,19 +96,20 @@ WSGI_APPLICATION = "can_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
+if not DEBUG and os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            engine="django.db.backends.postgresql",
+        )
+    }
+else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600,
-        )
     }
 
 
